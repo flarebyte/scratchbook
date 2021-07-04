79950bbbcbdc548011e3d1ca387b6aeedbd399ef
Olivier Huin <olih@users.noreply.github.com>
2021-07-04 11:05:28 +0100

    basic interface

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,52 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
+interface ScratchNote {
+  id: string;
+  text: string;
+  url?: string;
+}
+
+interface ScratchBook {
+  notes: ScratchNote[];
+}
+
+const createScratchNote = (
+  id: string,
+  text: string,
+  url?: string
+): ScratchNote => ({
+  id,
+  text,
+  url,
+});
+
+const mergeTwoScratchBook = (a: ScratchBook, b: ScratchBook): ScratchBook => {
+  return {
+    notes: [...a.notes, ...b.notes],
+  };
+};
+
+const mergeScratchBooks = (flatBooks: ScratchBook[]): ScratchBook => {
+  return flatBooks.reduce(mergeTwoScratchBook);
+};
+
+const byScratchNotePrefix = (prefix: string) => (
+  flatNote: ScratchNote
+): boolean => flatNote.id.startsWith(prefix);
+
+const byScratchNoteSuffix = (suffix: string) => (
+  flatNote: ScratchNote
+): boolean => flatNote.id.endsWith(suffix);
+
+const byScratchNoteSuffixList = (suffixList: string[]) => (
+  flatNote: ScratchNote
+): boolean =>
+  suffixList.map(suffix => flatNote.id.endsWith(suffix)).some(b => b);
+
+export {
+  ScratchNote,
+  ScratchBook,
+  createScratchNote,
+  mergeScratchBooks,
+  byScratchNotePrefix,
+  byScratchNoteSuffix,
+  byScratchNoteSuffixList,
 };
---
+++ test/main.test.ts
@@ -0,0 +1,10 @@
+import { createScratchNote } from '../src';
+
+describe('create some scratch note', () => {
+  it('create a simple scratch note', () => {
+    expect(createScratchNote('some-id', 'some text')).toBeDefined();
+  });
+  it('create a scratch note with link', () => {
+    expect(createScratchNote('some-id', 'some text', 'https://github.com/flarebyte/scratchbook')).toBeDefined();
+  });
+});
