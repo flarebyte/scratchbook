9dd0b625a0877d141b946aac94e278749b56138b
Olivier Huin <olih@users.noreply.github.com>
2021-07-05 09:29:17 +0100

    Adds tests for newScratchNote

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,4 +1,6 @@
-import { createScratchNote } from '../src';
+import { createScratchNote, newScratchNote, compose } from '../src';
+
+const slash = compose('/');
 
 describe('create some scratch note', () => {
   it('create a simple scratch note', () => {
@@ -12,5 +14,38 @@ describe('create some scratch note', () => {
         'https://github.com/flarebyte/scratchbook'
       )
     ).toBeDefined();
+  });
+});
+
+describe('create some scratch note with composer', () => {
+  it.each([
+    {
+      parts: [],
+      text: 'some text',
+      url: undefined,
+      id: '',
+    },
+    {
+      parts: ['bird'],
+      text: 'Eagle',
+      url: undefined,
+      id: 'bird',
+    },
+    {
+      parts: ['bird', 'predator'],
+      text: 'Eagle',
+      url: undefined,
+      id: 'bird/predator',
+    },
+    {
+      parts: ['bird', 'predator', 'worldwide'],
+      text: 'Eagle',
+      url: undefined,
+      id: 'bird/predator/worldwide',
+    },
+  ])('create a scratch note with $text, $url', ({ parts, text, url, id }) => {
+    const actual = newScratchNote(slash)(parts, text, url);
+    expect(actual).toBeDefined();
+    expect(actual.id).toBe(id);
   });
 });
